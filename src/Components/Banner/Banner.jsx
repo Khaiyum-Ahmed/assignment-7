d092115fdff32b194ec1cc5c270d7d2ce5a28f10
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-07-26 18:05:57 +0600

    hero section added

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,17 @@
+import hero from '../../assets/assets/banner-main.png'
+import shadow from '../../assets/assets/bg-shadow.png'
+const Banner = () => {
+    return (
+        <div className="max-w-11/12 mx-auto mb-20">
+            <div className='text-center py-16 rounded-3xl bg-[#131313] relative'>
+                <img className='absolute w-full h-full top-0 left-0 rounded-3xl' src={shadow} alt="" />
+                <img className='mx-auto mb-6' src={hero} alt="" />
+                <h1 className='mb-4 font-bold text-2xl lg:text-[40px] text-white'>Assemble Your Ultimate Dream 11 Cricket Team</h1>
+                <p className='mb-6 font-medium text-[rgba(255,255,255,.7)] lg:text-2xl text-base'>Beyond Boundaries Beyond Limits</p>
+                <button className='font-bold text-base text-[#131313] py-3 px-5 bg-[#E7FE29] rounded-xl'>Claim Free Credit</button>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,23 +1,28 @@
 import logo from '../../assets/assets/logo.png'
+import { FaBars } from "react-icons/fa";
 import { TbCoinTaka } from "react-icons/tb";
 const Header = () => {
     return (
         <header className="max-w-11/12 mx-auto my-6">
-            <div className='flex justify-between items-center'>
+            {/* Nav bar section */}
+            <nav className='flex justify-between items-center'>
                 <img src={logo} alt="" />
                 <div className='flex justify-center items-center'>
-                    <ul className='flex space-x-12 text-base text-[rgba(19,19,19,.7)]'>
+                    <ul className='md:flex hidden space-x-12 text-base text-[rgba(19,19,19,.7)]'>
                         <li><a href='#'>Home</a></li>
                         <li><a href='#'>Fixture</a></li>
                         <li><a href='#'>Team</a></li>
                         <li><a href='#'>Schedules</a></li>
                     </ul>
+                    <div className='md:hidden sm:block'>
+                        <span><FaBars /></span>
+                    </div>
                     <div className='ml-6'>
                         <button className='flex justify-center items-center text-base font-semibold text-[#131313] py-3 px-6 border border-[rgba(19,19,19,.1)] rounded-xl'>Taka <span className='ml-2 text-xl text-[#131313]'><TbCoinTaka /></span></button>
                     </div>
                 </div>
                 
-            </div>
+            </nav>
         </header>
     );
 };
